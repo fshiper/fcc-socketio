4439d7463e1646816523ff57a13b338c19d24148
Glitch (fshiper-fcc-socketio) <none>
2020-03-01 21:23:25 +0000

    💈💾 Updated with Glitch

--- server.js
+++ server.js
@@ -1,50 +1,76 @@
-'use strict';
+"use strict";
 
-const express     = require('express');
-const session     = require('express-session');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const auth        = require('./app/auth.js');
-const routes      = require('./app/routes.js');
-const mongo       = require('mongodb').MongoClient;
-const passport    = require('passport');
-const cookieParser= require('cookie-parser')
-const app         = express();
-const http        = require('http').Server(app);
-const sessionStore= new session.MemoryStore();
-
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const auth = require("./app/auth.js");
+const routes = require("./app/routes.js");
+const mongo = require("mongodb").MongoClient;
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const sessionStore = new session.MemoryStore();
+const cors = require("cors");
+const passportSocketIo = require("passport.socketio");
+app.use(cors());
 
 fccTesting(app); //For FCC testing purposes
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'pug')
+app.set("view engine", "pug");
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  key: 'express.sid',
-  store: sessionStore,
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    key: "express.sid",
+    store: sessionStore
+  })
+);
 
+mongo.connect(process.env.DATABASE, (err, client) => {
+  if (err) console.log("Database error: " + err);
+  let db = client.db("myproject");
+  console.log("Successful database connection");
 
-mongo.connect(process.env.DATABASE, (err, db) => {
-    if(err) console.log('Database error: ' + err);
-  
-    auth(app, db);
-    routes(app, db);
+  auth(app, db);
+  routes(app, db);
+  let currentUsers = 0;
+  http.listen(process.env.PORT || 3000);
+
+  //start socket.io code
+  io.use(
+    passportSocketIo.authorize({
+      cookieParser: cookieParser,
+      key: "express.sid",
+      secret: process.env.SESSION_SECRET,
+      store: sessionStore
+    })
+  );
+
+  io.on("connection", socket => {
+    
+    console.log("user " + socket.request.user.name + " connected");
+    ++currentUsers;
+    
+    io.emit("user", {name: socket.request.user.name,currentUsers: currentUsers,connected: true});
+    socket.on('chat message', (message) => {
+      io.emit("chat message", {name:socket.request.user.name, message:message})
+    });
+    socket.on("disconnect", () => {
+      console.log("user " + socket.request.user.name + " disconnected");
+      --currentUsers;
       
-    http.listen(process.env.PORT || 3000);
+      io.emit("user", {name: socket.request.user.name,currentUsers: currentUsers,connected: false});
+    });
+  });
 
-  
-    //start socket.io code  
-
-  
-
-    //end socket.io code
-  
-  
+  //end socket.io code
 });
